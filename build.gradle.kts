800aa504c878c9f5570e0602e2b3239d0f020507
Alan Hughes <hughesno1@gmail.com>
2022-08-20 22:11:53 +0100

    Remove Main.kt

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,9 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
+    java
     kotlin("jvm") version "1.7.10"
+    `maven-publish`
 }
 
 group = "com.alanhughes"
@@ -21,4 +23,16 @@ tasks.test {
 
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "com.alanhughes"
+            artifactId = "interpolation-kt"
+            version = "0.1.0"
+
+            from(components["java"])
+        }
+    }
 }
